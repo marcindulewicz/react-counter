50355d987bd1645a2af468903b97fdafb0a9fb90
marcindulewicz <dulewiczm@gmail.com>
2020-01-19 09:51:34 +0100

    Pusta aplikacja

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Btn from './button.js'
 
 function App() {
   return (
@@ -18,7 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <Btn napis1="Jestem pierwszy"/>
+        <Btn napis1="Jestem drugi"/>
       </header>
+      
     </div>
   );
 }
